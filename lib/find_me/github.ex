40ef6c94960840614b69b9e96087d2dfa3f822be
noordean <ebroyeem90@gmail.com>
2018-12-24 09:45:11 +0100

    add exjsx to decode JSON response

--- lib/find_me/github.ex
+++ lib/find_me/github.ex
@@ -6,10 +6,15 @@ defmodule FindMe.Github do
   def fetch(project, query, count) do
     HTTPoison.get("https://api.github.com/search/issues?q=#{query}+type:pr+user:#{project}", @headers)
       |> handle_response
+      |> extract_json_response
       |> IO.inspect
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, body }
+  def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, JSX.decode(body) }
 
-  def handle_response({:ok, %{status_code: _, body: body}}), do: { :error, body }
+  def handle_response({:ok, %{status_code: _, body: body}}), do: { :error, JSX.decode(body) }
+
+  def extract_json_response({:ok, {:ok, json_results}}), do: json_results
+
+  def extract_json_response({:error, {:ok, json_error}}), do: json_error
 end
