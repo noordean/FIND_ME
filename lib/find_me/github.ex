292279342cd04151af6a423cfbfef5d0bba8bc58
noordean <ebroyeem90@gmail.com>
2018-12-26 10:46:23 +0100

    extract result data

--- lib/find_me/github.ex
+++ lib/find_me/github.ex
@@ -7,7 +7,7 @@ defmodule FindMe.Github do
     HTTPoison.get("https://api.github.com/search/issues?q=#{query}+type:pr+user:#{project}", @headers)
       |> handle_response
       |> extract_json_response
-      |> IO.inspect
+      |> extract_result_data
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}), do: { :ok, JSX.decode(body) }
@@ -28,5 +28,29 @@ defmodule FindMe.Github do
       |> IO.puts
 
     System.halt(2)
+  end
+
+  def extract_result_data(json_results) do
+    IO.inspect Enum.map(json_results["items"], fn(result) ->
+      matches = result["text_matches"] |> Enum.at(0)
+      object_url = matches |> Map.get("object_url")
+      fragment = matches |> Map.get("fragment")
+      comment_number = extract_comment_number(object_url)
+
+      %{
+        "Date" => result["created_at"],
+        "Link" => "#{result["html_url"]}/#{comment_number}",
+        "User" => result["user"]["login"],
+        "Content" => fragment
+      }
+    end)
+  end
+
+  def extract_comment_number(object_url) do
+    split_object_url = String.split(object_url, "/")
+    comment = split_object_url |> Enum.at(-2)
+    if comment == "comments" do
+      "#discussion_r#{Enum.at(split_object_url, -1)}"
+    end
   end
 end
